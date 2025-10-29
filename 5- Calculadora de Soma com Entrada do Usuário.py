49e0c86bf274bfe631c909a06aaeb928723cae94
Paulo-Nisti <paulonisti@gmail.com>
2025-10-29 15:43:56 -0300

    Create 5- Calculadora de Soma com Entrada do Usuário.py

---
+++ 5- Calculadora de Soma com Entrada do Usuário.py
@@ -0,0 +1,16 @@
+# Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+# Efetue a soma de A e B, atribuindo o seu resultado à variável X. 
+# Entrada: A entrada contém 2 valores inteiros informados pelo usuário. 
+# Saída: Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha.
+
+# 1. Leitura do primeiro valor inteiro
+A = int(input("Digite o primeiro valor: "))
+
+# 2. Leitura do segundo valor inteiro
+B = int(input("Digite o segundo valor: "))
+
+# 3. Efetua a soma e armazena o resultado em X
+X = A + B
+
+# 4. Imprime o resultado no formato "X = [soma]"
+print(f"X = {X}")
